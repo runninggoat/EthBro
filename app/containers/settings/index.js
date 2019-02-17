a0285994e3bc08e74e6ad00ae8ae5434a3a582b9
runninggoat <1501832474@qq.com>
2019-02-17 21:29:17 +0800

    fix: modify gradle file to make project run on my linux machine

--- app/containers/settings/index.js
+++ app/containers/settings/index.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import {
   View,
   Text,
+  Image,
 } from 'react-native'
 import { connect } from 'react-redux'
+import Images from '../../images'
 
 class Settings extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { settings,  settingsFocused} = Images
+    return {
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        return (
+          <Image source={focused ? settingsFocused : settings} style={{ width: 24, height: 24 }}/>
+        )
+      },
+      tabBarLabel: '设置',
+      swipeEnabled: true,
+      animationEnabled: true,
+    }
+  }
+
   render() {
     return (
       <View>
